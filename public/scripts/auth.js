25d765ef2aff0ac285ef660d9edcb48a610eda2b
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2020-11-24 18:36:02 -0800

    Updates Files

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,64 +1,120 @@
-const loginForm = document.querySelector('#signIn-form');
-const logout = document.querySelector('#logout');
-const signUpForm = document.querySelector("#signUp-form");
 
-const password = loginForm['signInStudentPassword'].value;
-console.log(password)
-//listen for changes
-auth.onAuthStateChanged(user => {
-    if(user){
-        console.log("User logged in: ", user)
-    }
-    else{
-        console.log("User logged out")
-    }
-});
+//Student Sign Up
+const signUpform = document.querySelector('#signUp-form');
+signUpform.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-//Signing Up
-signUpForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    //get user info
+    const firstName = signUpform['signUpFirstName'].value;
+    const lastName = signUpform['signUpLastName'].value;
+    const id = signUpform['signUpStudentID'].value;
+    const email = signUpform['signUpStudentEmail'].value;
+    const password = signUpform['signUpStudentPassword'].value;
 
-    //User Info
-    const email = signUpForm['signUpStudentEmail'].value;
-    const password = signUpForm['signUpStudentPassword'].value;
-    console.log(email + password)
-
+    //SignUp for Students
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        return db.collection('students').doc(cred.user.uid).set({
-            email ,
-            password,
-            studentName: signUpForm['signUpStudentName'].value,
-            studentID: signUpForm['signUpStudentID'].value
-          });
-        console.log("Created -> ", cred);
-
-        // Closing the signup modal & reset form
-        
-    });
-})
-
-
-// // logout
-// logout.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     auth.signOut().then(() => {
-//         //Promise 
-
-//     })
-// });
-
-
-// login
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // get user info
-    const email = loginForm['signInStudentEmail'].value;
-    const password = loginForm['signInStudentPassword'].value;
-    console.log(email + password)
-    // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        // close the signup modal & reset form
-       
-    });
+        db.collection('students').doc(cred.user.uid).set({
+            firstName,
+            lastName,
+            uid: cred.user.uid,
+            id,
+            email,
+            joinDate: (new Date()).getTime()
+          }).then(() => {
+            $('#signUpModal').modal('hide');
+            signUpform.reset();
+            window.open("http://127.0.0.1:5500/public/studentPortal.html");
+            
+        });
 });
+});
+
+const adminSignInForm = document.querySelector("#adminSignIn-form");
+adminSignInForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = adminSignInForm['adminSignInEmail'].value;
+    const password = adminSignInForm['adminSignInPassword'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log('cred')
+        console.log(cred)
+        console.log('cred.user')
+        console.log(cred.user);
+        db.collection("admins").where("uid", "==", cred.user.uid)
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+
+                });
+            })
+        $('#signInModal').modal('hide');
+
+        signInForm.reset();
+    });
+
+
+});
+
+
+
+//Student Sign In form
+const signInForm = document.querySelector('#signIn-form');
+signInForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = signInForm['signInStudentEmail'].value;
+    const password = signInForm['signInStudentPassword'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log('cred')
+        console.log(cred)
+        console.log('cred.user')
+        console.log(cred.user);
+        db.collection("users").where("uid", "==", cred.user.uid)
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+
+                });
+            })
+        $('#signInModal').modal('hide');
+
+        signInForm.reset();
+    });
+
+
+});
+
+
+function userSignIn() {
+    $('#signUpModal1').parent().hide();
+    $('#signInModal1').parent().hide();
+    $('#logoutBtn1').parent().show();
+    if (Cookies.get('userType') == 'Student') {
+        $('#bookAppointment1').parent().show();
+        $('#provideAvailability1').parent().hide();
+    }
+    else if (Cookies.get('userType') == 'Admin') {
+        $('#bookAppointment1').parent().hide();
+        $('#provideAvailability1').parent().show();
+    }
+}
+
+
+function userSignOut() {
+    $('#signUpModal1').parent().show();
+    $('#signInModal1').parent().show();
+    $('#logoutBtn1').parent().hide();
+    $('#bookAppointment1').parent().hide();
+    $('#provideAvailability1').parent().hide();
+}
+
+function loginLogout() {
+    if (Cookies.get('email') == undefined)
+        userSignOut();
+    else
+        userSignIn();
+}
