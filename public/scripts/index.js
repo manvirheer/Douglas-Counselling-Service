73c741a2adb091ed1a10c5c787b1f558a6571b12
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2020-11-27 16:22:28 -0800

    Update Files

--- public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const studentsModal = document.querySelector("#studentsModal");
-const counsellorModal = document.querySelector("#counsellorModal");
-
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     var modals = document.querySelectorAll('.modal');
-//     M.Modal.init(modals);
-
-// });
