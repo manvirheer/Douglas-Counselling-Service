f8a3bf88526594e5bc9ce982eb7055f8ca8ea606
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2020-11-29 21:23:38 -0800

    Add the video function

--- public/scripts/counsellorPortal.js
+++ public/scripts/counsellorPortal.js
@@ -1,39 +1,75 @@
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    //  console.log("User logged in: ", user)
+    console.log("Logged In user ID -> " + user.uid);
+    db.collection("admins")
+      .doc(user.uid)
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data().firstName);
+          const counsellorGreetings = document.querySelector(
+            "#counsellorGreetings"
+          );
+          counsellorGreetings.innerHTML =
+            "Welcome, " + doc.data().firstName + "!";
 
-auth.onAuthStateChanged(user => {
-    if (user) {
-        
-        //  console.log("User logged in: ", user)
-        console.log("Logged In user ID -> " + user.uid)
-        db.collection('admins').doc(user.uid).get().then(function (doc) {
-            if (doc.exists) {
-                console.log("Document data:", doc.data().firstName);
-                const counsellorGreetings = document.querySelector("#counsellorGreetings");
-                counsellorGreetings.innerHTML = "Welcome, " + doc.data().firstName + "!";
-
-                db.collection('student-bookings').onSnapshot(snapshot => {
-                    var table = document.querySelector("#tbl-bookedAp")
-                    //Handle the latest event
-                    snapshot.forEach(doc => {
-                        if(user.uid == doc.data().admin_id){
-                        db.collection("admin-availability").onSnapshot(snapshot => {
-                            snapshot.forEach(docInner => {
-                                if (docInner.id == doc.data().admin_availability) {
-                                    var type = docInner.data().type;
-                                    var startDate = new Date(docInner.data().startTime.toDate()).toLocaleDateString("en-US");
-                                    var startTime = new Date(docInner.data().startTime.toDate()).toLocaleTimeString("en-US");
-                                    var finishDate = new Date(docInner.data().finishTime.toDate()).toLocaleDateString("en-US");
-                                    var finishTime = new Date(docInner.data().finishTime.toDate()).toLocaleTimeString("en-US");
-                                    db.collection('students').doc(doc.data().student_id).get().then(docSpec => {
-                                        console.log(doc.data().student_id);
-                                        var name = docSpec.data().id; //this is different from student_id, student_id is cCZIW31A3rw
-                                        console.log(type + "Time = " + startTime + " Time =" + finishTime + "Name" + name)
-                                        window.localStorage.setItem("roomIDGlobal",doc.data().admin_availability);
-                                        var varr = user.uid;
-                                        window.localStorage.setItem("userID",varr);
-                                        var studID = doc.data().student_id;
-                                        window.localStorage.setItem("roomEmailStudentGlobal",studID);
-                                        console.log("sid "+studID+"aid "+user.uid+"mid "+doc.id);
-                                        table.innerHTML += `<tr>
+          db.collection("student-bookings").onSnapshot((snapshot) => {
+            var table = document.querySelector("#tbl-bookedAp");
+            //Handle the latest event
+            snapshot.forEach((doc) => {
+              if (user.uid == doc.data().admin_id) {
+                db.collection("admin-availability").onSnapshot((snapshot) => {
+                  snapshot.forEach((docInner) => {
+                    if (docInner.id == doc.data().admin_availability) {
+                      var type = docInner.data().type;
+                      var startDate = new Date(
+                        docInner.data().startTime.toDate()
+                      ).toLocaleDateString("en-US");
+                      var startTime = new Date(
+                        docInner.data().startTime.toDate()
+                      ).toLocaleTimeString("en-US");
+                      var finishDate = new Date(
+                        docInner.data().finishTime.toDate()
+                      ).toLocaleDateString("en-US");
+                      var finishTime = new Date(
+                        docInner.data().finishTime.toDate()
+                      ).toLocaleTimeString("en-US");
+                      db.collection("students")
+                        .doc(doc.data().student_id)
+                        .get()
+                        .then((docSpec) => {
+                          console.log(doc.data().student_id);
+                          var name = docSpec.data().id; //this is different from student_id, student_id is cCZIW31A3rw
+                          console.log(
+                            type +
+                              "Time = " +
+                              startTime +
+                              " Time =" +
+                              finishTime +
+                              "Name" +
+                              name
+                          );
+                          window.localStorage.setItem(
+                            "roomIDGlobal",
+                            doc.data().admin_availability
+                          );
+                          var varr = user.uid;
+                          window.localStorage.setItem("userID", varr);
+                          var studID = doc.data().student_id;
+                          window.localStorage.setItem(
+                            "roomEmailStudentGlobal",
+                            studID
+                          );
+                          console.log(
+                            "sid " +
+                              studID +
+                              "aid " +
+                              user.uid +
+                              "mid " +
+                              doc.id
+                          );
+                          table.innerHTML += `<tr>
                         <td>${type}</td>
                         <td>
                             <p class="h4">${startDate} at ${startTime} - ${finishTime}</p>
@@ -41,89 +77,89 @@ auth.onAuthStateChanged(user => {
                         <td>${name}</td>
                         <td><a href="createChat.html?studentID=${studID}&adminID=${user.uid}&meetingID=${doc.id}">BUTTON<a></td>
                     </tr>`;
-                                        
-                                    })
-                                }
-                            })
-                        })
+                        });
                     }
-                    })
-
-                })
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-        const timeForm = document.querySelector('#availability-form');
-        timeForm.addEventListener("submit", function(e){
-            e.preventDefault();
-            const startTime = document.getElementById("startTimeid").value;
-            const finishTime = document.getElementById("finishTimeid").value;
-            const type = timeForm["type"].value;
-            console.log(Date.parse(startTime)/1000);
-            console.log(startTime);
-            console.log("hey");
-            db.collection("admin-availability").add({
-               admin_id : user.uid,
-               "type": type,
-               startTime : Date.parse(startTime)/1000,
-               finishTime : Date.parse(finishTime)/1000
-                
-            })
-            .then(function(docRef) {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function(error) {
-                console.error("Error adding document: ", error);
+                  });
+                });
+              }
             });
+          });
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+    const timeForm = document.querySelector("#availability-form");
+    timeForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const startTime = document.getElementById("startTimeid").value;
+      const finishTime = document.getElementById("finishTimeid").value;
+      const counsellorZoomLink = document.getElementById("counsellorZoomLink")
+        .value;
+      const type = timeForm["type"].value;
+      console.log(Date.parse(startTime) / 1000);
+      console.log(startTime);
+      console.log("hey");
+      db.collection("admin-availability")
+        .add({
+          admin_id: user.uid,
+          type: type,
+          startTime: Date.parse(startTime) / 1000,
+          finishTime: Date.parse(finishTime) / 1000,
+          counsellorZoomLink,
         })
-        
-    }
-    else {
-        console.log("User logged out - No user in admin portal")
-    }
+        .then(function (docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
+        });
+    });
+  } else {
+    console.log("User logged out - No user in admin portal");
+  }
 });
 
 const logoutBtn = document.querySelector("#logoutBtn");
 
 logoutBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    auth.signOut();
-    window.open("index.html", "_self");
-})
+  e.preventDefault();
+  auth.signOut();
+  window.open("index.html", "_self");
+});
 
 //Admin Sign Up Form
-const signUpform = document.querySelector('#add-form');
-signUpform.addEventListener('submit', (e) => {
-    e.preventDefault()
+const signUpform = document.querySelector("#add-form");
+signUpform.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    //get user info
-    const firstName = signUpform['signUpFirstName'].value;
-    const lastName = signUpform['signUpLastName'].value;
-    const phone = signUpform['signUpPhoneNumber'].value;
-    const year = signUpform['signUpYear'].value;
-    const email = signUpform['signInStudentEmail'].value;
-    const password = signUpform['signInStudentPassword'].value;
-    
-    //SignUp for Students
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        db.collection('admins').doc(cred.user.uid).set({
-            firstName,
-            lastName,
-            uid: cred.user.uid,
-            year,
-            phone,
-            email,
-            joinDate: (new Date()).getTime()
-          }).then(() => {
-            $('#signUpModal').modal('hide');
-            signUpform.reset();
-            
-        });
+  //get user info
+  const firstName = signUpform["signUpFirstName"].value;
+  const lastName = signUpform["signUpLastName"].value;
+  const phone = signUpform["signUpPhoneNumber"].value;
+  const year = signUpform["signUpYear"].value;
+  const email = signUpform["signInStudentEmail"].value;
+  const password = signUpform["signInStudentPassword"].value;
+
+  //SignUp for Students
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    db.collection("admins")
+      .doc(cred.user.uid)
+      .set({
+        firstName,
+        lastName,
+        uid: cred.user.uid,
+        year,
+        phone,
+        email,
+        joinDate: new Date().getTime(),
+      })
+      .then(() => {
+        $("#signUpModal").modal("hide");
+        signUpform.reset();
+      });
+  });
 });
-});
-
-
--- public/scripts/studentPortal.js
+++ public/scripts/studentPortal.js
@@ -1,77 +1,119 @@
+// the Zoom link has to be the admin's personal Zoom Link
+// we can get this from the admin itself
+// note: get the zoom link from your profile so change the dafault link below
+const ZOOM_LINK = "https://us04web.zoom.us/profile";
 
-auth.onAuthStateChanged(user => {
-    if (user) {
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    //  console.log("User logged in: ", user)
+    console.log("Logged In user ID -> " + user.uid);
+    db.collection("students")
+      .doc(user.uid)
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data().firstName);
+          const studentGreetings = document.querySelector("#studentGreetings");
+          studentGreetings.innerHTML = "Welcome, " + doc.data().firstName + "!";
 
-        //  console.log("User logged in: ", user)
-        console.log("Logged In user ID -> " + user.uid)
-        db.collection('students').doc(user.uid).get().then(function (doc) {
-            if (doc.exists) {
-                console.log("Document data:", doc.data().firstName);
-                const studentGreetings = document.querySelector("#studentGreetings");
-                studentGreetings.innerHTML = "Welcome, " + doc.data().firstName + "!";
+          db.collection("student-bookings").onSnapshot((snapshot) => {
+            var table = document.querySelector("#tbl-bookedAp");
+            //Handle the latest event
+            snapshot.forEach((doc) => {
+              console.log("hi");
+              //console.log(doc.data())
+              //console.log(`User ID = ${user.uid} ${doc.data().student_id}`);
+              //console.log("bruh")
+              if (user.uid == doc.data().student_id) {
+                db.collection("admin-availability").onSnapshot((snapshot) => {
+                  snapshot.forEach((docInner) => {
+                    if (docInner.id == doc.data().admin_availability) {
+                      var type = docInner.data().type;
+                      var startDate = new Date(
+                        docInner.data().startTime
+                      ).toLocaleDateString("en-US");
+                      var startTime = new Date(
+                        docInner.data().startTime
+                      ).toLocaleTimeString("en-US");
+                      var finishDate = new Date(
+                        docInner.data().finishTime
+                      ).toLocaleDateString("en-US");
+                      var finishTime = new Date(
+                        docInner.data().finishTime
+                      ).toLocaleTimeString("en-US");
+                      const counsellorZoomLink = docInner.data()
+                        .counsellorZoomLink;
+                      db.collection("admins")
+                        .doc(docInner.data().admin_id)
+                        .get()
+                        .then((docSpec) => {
+                          var name = docSpec.data().firstName;
+                          console.log(
+                            type +
+                              "Time = " +
+                              startTime +
+                              " Time =" +
+                              finishTime +
+                              "Name" +
+                              name
+                          );
 
-                db.collection('student-bookings').onSnapshot(snapshot => {
-                    var table = document.querySelector("#tbl-bookedAp")
-                    //Handle the latest event
-                    snapshot.forEach(doc => {
-                        console.log("hi")
-                        //console.log(doc.data())
-                        //console.log(`User ID = ${user.uid} ${doc.data().student_id}`);
-                        //console.log("bruh")
-                        if(user.uid == doc.data().student_id){
-                        db.collection("admin-availability").onSnapshot(snapshot => {
-                            snapshot.forEach(docInner => {
-                                if ((docInner.id == doc.data().admin_availability)){
-                                    var type = docInner.data().type;
-                                    var startDate = new Date(docInner.data().startTime.toDate()).toLocaleDateString("en-US");
-                                    var startTime = new Date(docInner.data().startTime.toDate()).toLocaleTimeString("en-US");
-                                    var finishDate = new Date(docInner.data().finishTime.toDate()).toLocaleDateString("en-US");
-                                    var finishTime = new Date(docInner.data().finishTime.toDate()).toLocaleTimeString("en-US");
-                                    db.collection('admins').doc(docInner.data().admin_id).get().then(docSpec => {
-                                        var name = docSpec.data().firstName
-                                        console.log(type + "Time = " + startTime + " Time =" + finishTime + "Name" + name)
-                                       
-                                       
-                                        window.localStorage.setItem("roomIDGlobal",doc.data().admin_availability);
-                                        var varr = user.uid;
-                                        console.log(doc.id)
-                                       
-                                        window.localStorage.setItem("userID",varr);
-                                        console.log(window.localStorage.getItem("userID"));
-                                        window.localStorage.setItem("roomEmailStudentGlobal",user.uid);
-                                        table.innerHTML += `<tr>
+                          window.localStorage.setItem(
+                            "roomIDGlobal",
+                            doc.data().admin_availability
+                          );
+                          var varr = user.uid;
+                          console.log(doc.id);
+
+                          window.localStorage.setItem("userID", varr);
+                          console.log(window.localStorage.getItem("userID"));
+                          window.localStorage.setItem(
+                            "roomEmailStudentGlobal",
+                            user.uid
+                          );
+
+                          let href;
+                          if (type.toUpperCase() === "CHAT") {
+                            href = `createChat.html?studentID=${user.uid}&adminID=${docSpec.id}&meetingID=${doc.id}`;
+                          } else {
+                            if (counsellorZoomLink.includes("zoom.us/")) {
+                              href = counsellorZoomLink;
+                            } else {
+                              href = ZOOM_LINK;
+                            }
+                          }
+
+                          table.innerHTML += `<tr>
                                             <td>${type}</td>
                                             <td>
                                                 <p class="h4">${startDate} at ${startTime} - ${finishTime}</p>
                                             </td>
                                             <td>Dr. ${name}</td>
-                                            <td><a href="createChat.html?studentID=${user.uid}&adminID=${docSpec.id}&meetingID=${doc.id}">BUTTON<a></td>
+                                            <td><a href="${href}">BUTTON<a></td>
                                             </tr>`;
-                                        
-                                    })
-                                }
-                            })
-                        })
+                        });
                     }
-                    })
-
-                })
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-    }
-    else {
-        console.log("User logged out - No user in student portal")
-    }
+                  });
+                });
+              }
+            });
+          });
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+  } else {
+    console.log("User logged out - No user in student portal");
+  }
 });
 
 const logoutBtn = document.querySelector("#logoutBtn");
 
 logoutBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    auth.signOut();
-})
+  e.preventDefault();
+  auth.signOut();
+});
